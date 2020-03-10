d62691055990495ec59aea913ff74e201d4a8b0a
user1 <user1@user1-PC>
2020-03-10 17:49:34 +0900

    3/10 집가서 하기

--- WassupSeoul/src/main/java/com/kh/wassupSeoul/street/service/StreetService.java
+++ WassupSeoul/src/main/java/com/kh/wassupSeoul/street/service/StreetService.java
@@ -1,5 +1,9 @@
 package com.kh.wassupSeoul.street.service;
 
+import com.kh.wassupSeoul.street.model.vo.Street;
+
 public interface StreetService {
 
+	public abstract Street selectStreet() throws Exception;
+	 
 }
