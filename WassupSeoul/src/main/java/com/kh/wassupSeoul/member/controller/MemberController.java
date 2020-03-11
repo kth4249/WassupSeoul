a27929acbab68294b2dde9440c65c3c146be88f7
JH-11 <56540163+JH-11@users.noreply.github.com>
2020-03-11 17:49:08 +0900

    20200311 학원 커밋

--- WassupSeoul/src/main/java/com/kh/wassupSeoul/member/controller/MemberController.java
+++ WassupSeoul/src/main/java/com/kh/wassupSeoul/member/controller/MemberController.java
@@ -1,12 +1,15 @@
 
 package com.kh.wassupSeoul.member.controller;
 
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -14,49 +17,43 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.kh.wassupSeoul.member.model.vo.Member;
 import com.kh.wassupSeoul.member.service.MemberService;
 
-@SessionAttributes({"loginMember", "msg"})
+@SessionAttributes({ "loginMember", "msg" })
 @RequestMapping("/member/*")
 @Controller
-public class MemberController{
- 
+public class MemberController {
+
 	@Autowired
 	private MemberService memberService;
-	
+
 	// 약관 동의 페이지 이동
 	@RequestMapping("agreeForm")
 	public String agreeForm() {
 		return "/member/agreeForm";
 	}
-	
+
 	// 회원가입 페이지 이동
 	@RequestMapping("signUpForm")
 	public String signUpForm() {
 		return "/member/signUpForm";
 	}
-	
+
 	// 회원가입
 	@RequestMapping("signUp")
-	public String signUp(Member member, Model model, 
-			String phone1, String phone2, String phone3,
+	public String signUp(Member member, Model model, String phone1, String phone2, String phone3,
 			RedirectAttributes rdAttr) {
-		
+
 		String memberPhone = phone1 + "-" + phone2 + "-" + phone3;
-		
-		Member signUpMember = new Member(member.getMemberEmail(), 
-				member.getMemberPwd(), 
-				member.getMemberNm(),
-				member.getMemberNickname(),
-				memberPhone,
-				member.getMemberGender(),
-				member.getMemberAge());
-		
+
+		Member signUpMember = new Member(member.getMemberEmail(), member.getMemberPwd(), member.getMemberNm(),
+				member.getMemberNickname(), memberPhone, member.getMemberGender(), member.getMemberAge());
+
 		System.out.println(signUpMember);
-		
+
 		try {
 			int result = memberService.signUp(signUpMember);
 			String msg = null;
-			
-			if(result > 0) {
+
+			if (result > 0) {
 				msg = "가입성공";
 				rdAttr.addFlashAttribute("msg", msg);
 				return "redirect:/";
@@ -65,47 +62,65 @@ public class MemberController{
 				rdAttr.addFlashAttribute("msg", msg);
 				return "redirect:/";
 			}
-		
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			model.addAttribute("errorMsg", "회원 가입 과정에서 오류 과정");
 			return "/common/errorPage";
 		}
-		
+
 	}
-	
+
 	// 로그인
-	@RequestMapping(value="login", method=RequestMethod.POST) 
+	@RequestMapping(value = "login", method = RequestMethod.POST)
 	public String memberLogin(Member member, Model model, RedirectAttributes rdAttr) {
-		
+
 		try {
 			Member loginMember = memberService.loginMember(member);
 			String msg = null;
-			if(loginMember != null) {
+			if (loginMember != null) {
 				msg = "로그인 성공";
 				rdAttr.addFlashAttribute("msg", msg);
 				model.addAttribute("loginMember", loginMember);
-				return "redirect:/square"; 
+				return "redirect:/square";
 			} else {
 				msg = "로그인 실패";
 				rdAttr.addFlashAttribute("msg", msg);
 				return "redirect:/";
 			}
-			
-		} catch (Exception e)  {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 			model.addAttribute("errorMsg", "로그인 과정에서 오류 발생");
 			return "common/errorPage";
-		} 
-		
+		}
+
 	}
 
 	// 로그아웃
-	@RequestMapping("logout") 
+	@RequestMapping("logout")
 	public String memberLogout(SessionStatus status) {
 		status.setComplete();
 		return "redirect:/";
 	}
-	
+
+	// 이메일 찾기
+	@RequestMapping("findEmail")
+	public String findEmail(String name, String phone, Model model, HttpServletResponse response) {
+		Member member = new Member();
+		member.setMemberNm(name);
+		member.setMemberPhone(phone);
+
+		try {
+			String memberEmail = memberService.findEmail(member);
+			System.out.println(memberEmail);
+			
+			PrintWriter out = response.getWriter();
+			out.print(memberEmail);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 }
