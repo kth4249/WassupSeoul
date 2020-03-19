93f312c5ed5c975463361cf44f771de343fba727
kth4249 <desales4249@gmail.com>
2020-03-19 18:12:00 +0900

    충돌 해결

--- WassupSeoul/src/main/java/com/kh/wassupSeoul/street/model/dao/StreetDAO.java
+++ WassupSeoul/src/main/java/com/kh/wassupSeoul/street/model/dao/StreetDAO.java
@@ -94,87 +94,8 @@ public class StreetDAO {
 	}
 
 
-=======
 
 
-@Repository
-public class StreetDAO {
-
-	@Autowired	
-	private SqlSessionTemplate sqlSession;
-
-	/** 골목 조회용 DAO
-	 * @param streetNo
-	 * @return street
-	 * @throws Exception
-	 */
-	public Street selectStreet(Integer streetNo) throws Exception{
-		return sqlSession.selectOne("streetMapper.selectStreet", streetNo );
-	}
-
-	/** 게시글 조회용 DAO
-	 * @param streetNo
-	 * @return list
-	 * @throws Exception
-	 */
-	public List<Board> selectBoard(Integer streetNo) throws Exception{
-		return sqlSession.selectList("streetMapper.selectBoardList", streetNo );
-	}
-
-	/** 게시글 등록용 DAO
-	 * @param board
-	 * @return result
-	 * @throws Exception
-	 */
-	public int insertBoard(Board board) throws Exception {
-		return sqlSession.insert("streetMapper.insertBoard", board );
-	}
-
-	/** 좋아요 체크용 DAO
-	 * @param loginMember
-	 * @return result
-	 * @throws Exception
-	 */
-	public String likeCheck(Member loginMember) throws Exception {
-		return sqlSession.selectOne("streetMapper.likeCheck", loginMember );
-	}
-
-	/** 좋아요 기록용 DAO
-	 * @param loginMember
-	 * @return result
-	 * @throws Exception
-	 */
-	public int recordLike(Member loginMember) throws Exception {
-		
-		return sqlSession.insert("streetMapper.recordLike", loginMember );
-	}
-
-	/** 좋아요 업데이트용 DAO
-	 * @param loginMember
-	 * @return result
-	 * @throws Exception
-	 */
-	public int updateLike(Member loginMember) throws Exception {
-		return sqlSession.update("streetMapper.updateLike", loginMember );
-	}
-
-	/** 좋아요, 댓글수 조회용 DAO
-	 * @param postNo
-	 * @return 
-	 * @throws Exception
-	 */
-	public int[] checkLikeReplyNum(int postNo) throws Exception{
-		return sqlSession.selectOne("streetMapper.checkLikeReplyNum", postNo );
-	}
-
-	/** 게시글 삭제용 DAO
-	 * @param postNo
-	 * @return result
-	 * @throws Exception
-	 */
-	public int deletePost(int postNo) throws Exception{
-		return sqlSession.update("streetMapper.deletePost", postNo );
-	}
 
 	/** 골목 가입용 DAO
 	 * @param map
@@ -222,7 +143,6 @@ public class StreetDAO {
 	}
 
 
-}
 
 	/** 골목커버 다음 번호 조회용 DAO
 	 * @return imgNo
@@ -305,19 +225,6 @@ public class StreetDAO {
 		return sqlSession.insert("streetMapper.insertStreetKeyword", map);
 	}
 
-	
-
-	
-
-
-	/** 회원 관심사 조회용 DAO
-	 * @param memberNo
-	 * @return myHobby
-	 * @throws Exception
-	 */
-	public List<Hobby> selectHobby(int memberNo) throws Exception{
-		return sqlSession.selectList("memberMapper.selectHobby", memberNo);
-	}
 
 	
 	
