34e1f539b8012a7383d5d19d4e62f541a94ae700
디원곤듀 <@192.168.219.104>
2020-03-30 00:00:54 +0900

    2020.03.30 오전 12:00 김지원 commit

--- WassupSeoul/src/main/java/com/kh/wassupSeoul/admin/model/dao/AdminDAO.java
+++ WassupSeoul/src/main/java/com/kh/wassupSeoul/admin/model/dao/AdminDAO.java
@@ -6,7 +6,10 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kh.wassupSeoul.hobby.model.vo.Hobby;
 import com.kh.wassupSeoul.member.model.vo.Member;
+import com.kh.wassupSeoul.street.model.vo.Report;
+import com.kh.wassupSeoul.street.model.vo.Street;
 
 @Repository
 public class AdminDAO {
@@ -14,9 +17,50 @@ public class AdminDAO {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 
+	/** 회원 목록 조회용 DAO
+	 * @return mList
+	 * @throws Exception
+	 */
 	public List<Member> selectMemberList() throws Exception {
 		
 		return sqlSession.selectList("adminMapper.selectMemberList");
+	}
+
+	/** 골목 목록 조회용 DAO
+	 * @return sList
+	 * @throws Exception
+	 */
+	public List<Street> selectStreetList() throws Exception {
+		
+		return sqlSession.selectList("adminMapper.selectStreetList");
+	}
+
+	/** 전체 관심사 목록 조회용 DAO
+	 * @return hList
+	 * @throws Exception
+	 */
+	public List<Hobby> selectHobbyList() throws Exception {
+		
+		return sqlSession.selectList("adminMapper.selectHobbyList");
+	}
+
+	/** 활동보고서 목록 조회용 DAO
+	 * @return rList
+	 * @throws Exception
+	 */
+	public List<Report> selectReportList() throws Exception {
+		
+		return sqlSession.selectList("adminMapper.selectReportList");
+	}
+
+	/** 회원 강퇴용 DAO
+	 * @param memberNo
+	 * @return result
+	 * @throws Exception
+	 */
+	public int deleteMember(int memberNo) throws Exception {
+		
+		return sqlSession.update("adminMapper.deleteMember", memberNo);
 	}
 
 }
--- WassupSeoul/src/main/java/com/kh/wassupSeoul/admin/model/service/AdminServiceImpl.java
+++ WassupSeoul/src/main/java/com/kh/wassupSeoul/admin/model/service/AdminServiceImpl.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.wassupSeoul.admin.model.dao.AdminDAO;
+import com.kh.wassupSeoul.hobby.model.vo.Hobby;
 import com.kh.wassupSeoul.member.model.vo.Member;
+import com.kh.wassupSeoul.street.model.vo.Report;
+import com.kh.wassupSeoul.street.model.vo.Street;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -15,9 +18,57 @@ public class AdminServiceImpl implements AdminService {
 	private AdminDAO adminDAO;
 
 	
+	/** 회원 목록 조회용 Service
+	 * @return mList
+	 * @throws Exception
+	 */
 	@Override
 	public List<Member> selectMemberList() throws Exception {
 		
 		return adminDAO.selectMemberList();
+	}
+	
+	/** 골목 목록 조회용 Service
+	 * @return sList
+	 * @throws Exception
+	 */
+	@Override
+	public List<Street> selectStreetList() throws Exception {
+		
+		return adminDAO.selectStreetList();
+	}
+	
+	
+	
+	/** 전체 관심사 목록 조회용 Service
+	 * @return hList
+	 * @throws Exception
+	 */
+	@Override
+	public List<Hobby> selectHobbyList() throws Exception {
+		
+		return adminDAO.selectHobbyList();
+	}
+	
+	
+	/** 활동보고서 목록 조회용 Service
+	 * @return rList
+	 * @throws Exception
+	 */
+	@Override
+	public List<Report> selectReportList() throws Exception {
+		
+		return adminDAO.selectReportList();
+	}
+	
+	/** 회원 강퇴용 Service
+	 * @param memberNo
+	 * @return result
+	 * @throws Exception
+	 */
+	@Override
+	public int deleteMember(int memberNo) throws Exception {
+		
+		return adminDAO.deleteMember(memberNo);
 	}
 }
